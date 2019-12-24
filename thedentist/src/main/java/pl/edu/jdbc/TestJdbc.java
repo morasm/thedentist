32195de32916e8b721d41a07d4244b68e70e88de
Michał Morawski <moras.technika@gmail.com>
2019-12-24 11:42:41 +0100

    updated properties / database changed

--- thedentist/src/main/java/pl/edu/jdbc/TestJdbc.java
+++ thedentist/src/main/java/pl/edu/jdbc/TestJdbc.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class TestJdbc {
 
 	public static void main(String[] args) {
-		String jdbcUrl = "jdbc:postgresql://psql.serwer1905034.home.pl:5432/29624990_zpo_gs2019?serverTimezone=UTC";
-		String user = "29624990_zpo_gs2019";
-		String pass = "ThdB_gs_112019@edu";
+		String jdbcUrl = "jdbc:postgresql://...?serverTimezone=UTC";
+		String user = "...";
+		String pass = "...";
 		
 		try {
 			System.out.println("Connecting to: " + jdbcUrl);
